4ed3387cc9f5971f6040baee4cefd8ffe86a5273
Tom <tomxu.developer@gmail.com>
2024-09-06 15:48:37 +1000

    modify API

--- PracticeAPI/index.js
+++ PracticeAPI/index.js
@@ -4,52 +4,91 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-let task = [];
-let id = 0;
+let task = [
+  { id: 2, description: "task2", done: false },
+  { id: 1, description: "task1", done: false },
+  { id: 3, description: "task3", done: false },
+];
+let id = 4;
 app.get("/tasks", (req, res) => {
-  if (task.length === 0) {
-    return res.send("No task");
+  const {description} = req.query;
+
+  if (description) {
+    const findTask = task.filter((item)=>item.description.includes(description));
+    return res.json(findTask);
   }
-  return res.send(task);
+  return res.json(task);
 });
 app.get("/tasks/:id", (req, res) => {
-  const findID = req.params.id;
-  console.log(findID);
-  const foundData = task.filter((item) => {
-    console.log(item.id == findID);
-    return item.id == findID;
-  });
-  return res.send(foundData);
+  const findID = Number(req.params.id);
+  // const foundData = task.filter((item) => {
+  //   return item.id === findID;
+  // }); filter function will iterate the array, but for finding the item by ID, it can use find function
+
+  const foundData = task.find((item) => item.id === findID);
+  if (!foundData) {
+    res.status(404).json({
+      statusCode: 404,
+      error: "not found",
+      msg: "task not found",
+    });
+    return;
+  }
+  return res.json(foundData);
 });
 app.put("/tasks/:id", (req, res) => {
-  const findID = req.params.id;
-  const updateData = req.body;
-  const foundDataIndex = task.findIndex((item) => {
-    return item.id == findID;
-  });
-  task[foundDataIndex].description = updateData.description;
-  task[foundDataIndex].done = updateData.done;
-  return res.send(task[foundDataIndex]);
+  const findID = Number(req.params.id);
+  const foundData = task.find((item) => item.id === findID);
+  if (!foundData) {
+    res.status(404).json({
+      statusCode: 404,
+      error: "not found",
+      msg: "task not found",
+    });
+    return;
+  }
+  const { description, done } = req.body;
+  // const foundDataIndex = task.findIndex((item) => {
+  //   return item.id === findID;
+  // });
+  // task[foundDataIndex].description = updateData.description;
+  // task[foundDataIndex].done = updateData.done;
+  if (description !== undefined) {
+    foundData.description = description;
+  }
+  if (done !== undefined) {
+    foundData.done = done;
+  }
+  return res.json(foundData);
 });
 app.post("/tasks", (req, res) => {
-  id++;
-  const addData = req.body.description;
-  console.log(addData);
+  const { description } = req.body;
+  if (description === undefined) {
+    return res.status(404).json({
+      msg: "bad request",
+    });
+  }
   const newData = {
-    id: id,
-    description: addData,
+    id: id++,
+    description: description,
     done: false,
   };
   task.push(newData);
-  return res.send(task);
+  return res.status(201).json(task);
 });
 app.delete("/tasks/:id", (req, res) => {
-  const findID = req.params.id;
+  const findID = Number(req.params.id);
   const findDataIndex = task.findIndex((item) => {
-    return item.id == findID;
+    return item.id === findID;
   });
-  const deletedTask = task.splice(findDataIndex, 1);
-  return res.send(deletedTask);
+  if (findDataIndex === -1) {
+    res.status(404).json({
+      msg: "not found the task",
+    });
+    return;
+  }
+  const [deletedTask] = task.splice(findDataIndex, 1);
+  return res.status(204).json(deletedTask);
 });
 
 app.listen(3000, () => {
